28ae200d505aa53709c2af88312d12fc72038721
TSF404 <TSF404@users.noreply.github.com>
2019-04-13 14:50:22 +1000

    + Fixed Spawner/Pooler

--- Game Studio Project/Assets/Scripts/ArchitectureAlternative/GameManagers/GameSpawner.cs
+++ Game Studio Project/Assets/Scripts/ArchitectureAlternative/GameManagers/GameSpawner.cs
@@ -23,12 +23,32 @@ namespace AlternativeArchitecture {
 
         public void SpawnPlayer() {
             GameObject newPlayer = pooler.RetrieveOrCreate(ObjectType.PLAYER);
+            newPlayer.Show();
         }
 
         public void SpawnObject(ObjectType objectType)
         {
            GameObject spawnedObject = pooler.RetrieveOrCreate(objectType);
         }
+
+
+
+
+
+
+        // placeholder script for spawning objects over time
+        private float spawnCounter, spawnRate = 1;
+        private void FixedUpdate()
+        {
+            if (spawnCounter > spawnRate)
+            {
+                spawnCounter = 0;
+                SpawnObject(ObjectType.OBSTACLE_SPHERE);
+            }
+            spawnCounter += Time.deltaTime;
+        }
+
+
     }
 }
 

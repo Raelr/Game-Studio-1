dac5ada09e1bd94fffba3ac7cf8f521e091c7496
TSF_SeanSimon <thosesixfaces@gmail.com>
2019-04-16 09:29:27 +1000

    + Slight tuning to the look damping

--- Game Studio Project/Assets/Scripts/GameScripts/CameraScripts/CameraLookat.cs
+++ Game Studio Project/Assets/Scripts/GameScripts/CameraScripts/CameraLookat.cs
@@ -11,12 +11,21 @@ public class CameraLookat : MonoBehaviour
     {
         if (target)
         {
+      
             Transform camera = Camera.main.transform;
             Vector3 toTarget = target.position - camera.position;
             Quaternion targetRotation = Quaternion.LookRotation(toTarget);
-            camera.rotation = Quaternion.Lerp(camera.rotation, targetRotation, damping);
+            camera.rotation = Quaternion.Lerp(camera.rotation, targetRotation, damping * Time.deltaTime);
 
+            test = ""+Mathf.Round(camera.rotation.x*360) + ", " + Mathf.Round(camera.rotation.y * 360) + ", " + Mathf.Round(camera.rotation.z * 360);
         }
+    }
+
+    string test = "";
+
+    void OnGUI ()
+    {
+            GUI.Label(new Rect(10, 10, Screen.width, 20), test);
     }
 
     public void SetTarget (Transform newTarget)

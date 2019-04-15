1e32931820461965760bc42957951dbdc03847fd
rromano <rromanomusic@gmail.com>
2019-04-15 15:06:01 +1000

    Movement: Add Force
    Changed movement from position translation to adding force to rigidbody. Creating clamp system for screen bounds.

--- Game Studio Project/Assets/Scripts/ArchitectureAlternative/GlobalMethods.cs
+++ Game Studio Project/Assets/Scripts/ArchitectureAlternative/GlobalMethods.cs
@@ -19,4 +19,12 @@ public static class GlobalMethods {
 	public static Vector2 GetDirection(Vector2 a, Vector2 b) {
 		return (b - a).normalized;
 	}
+
+	/// <summary>
+	/// Returns true if value is within bounds.
+	/// </summary>
+	/// <returns><c>true</c>, if is within bounds, <c>false</c> otherwise.</returns>
+	public static bool WithinBounds(float value, float min, float max) {
+		return value > min && value < max;
+	}
 }
--- Game Studio Project/Assets/Scripts/ArchitectureAlternative/PlayerScripts/MovementController.cs
+++ Game Studio Project/Assets/Scripts/ArchitectureAlternative/PlayerScripts/MovementController.cs
@@ -11,8 +11,11 @@ public class MovementController : InitialisedEntity {
     PhysicsController physics;
 
 	[Header("Physics Properties")]
-	private float force = 1f;
+	private float force = 15f;
 	private float maxDistance = 8;
+
+	private Transform player;
+	private Vector2 screenBounds;
 
 	// Initialises all variables and gets the physics component. 
 	public override void Initialise() {
@@ -20,21 +23,27 @@ public class MovementController : InitialisedEntity {
         base.Initialise();
 
         physics = GetComponent<PhysicsController>();
-
+		player = transform.Find("Visuals");
+		screenBounds.x = Screen.width / 2;
+		screenBounds.y = Screen.height / 2;
         physics.Initialise();
     }
 
     // Makes all calculations for the physics and applies force via the physics component.
     public void MoveEntity(Vector2 targetPos) {
-		targetPos *= -1;
-		targetPos.y += 2;
+		//targetPos *= -1;
+		//targetPos.y += 2;
 
-		float dist = Vector3.Distance(targetPos, transform.position);
-		Vector2 dir = GlobalMethods.GetDirection(transform.position, targetPos);
+		float dist = Vector3.Distance(targetPos, player.position);
+		Vector2 dir = GlobalMethods.GetDirection(player.position, targetPos);
 		Vector2 velocity = dir * (force * (dist / maxDistance));
-		transform.position += new Vector3(velocity.x, velocity.y, 0);
+
+		velocity.x = GlobalMethods.WithinBounds(velocity.x, -screenBounds.x, screenBounds.x) ? velocity.x : 0;
+		velocity.y = GlobalMethods.WithinBounds(velocity.y, -screenBounds.y, screenBounds.y) ? velocity.y : 0;
+
+		//player.position += new Vector3(velocity.x, velocity.y, 0);
 
 		physics.AddForce(velocity);
     }
-
+    
 }

28cbc755e782ce40d1cd4675f1e230b1c36c2c40
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-04 19:41:07 +1000

    Player can shoot laser i one direction

--- Game Studio Project/Assets/Scripts/GameScripts/PlayerScripts/ProjectileController.cs
+++ Game Studio Project/Assets/Scripts/GameScripts/PlayerScripts/ProjectileController.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 // Manages the projectiles of the player
 public class ProjectileController : InitialisedEntity
 {
+    [SerializeField]
+    float timeBetweenShots = 2;
+
+
+    float timeSinceLastShot;
+
+    [SerializeField]
+    GameObject projectile;
+
     public override void Initialise() {
 
         base.Initialise();
+        timeSinceLastShot = 0;
     }
 
     // Fires projectiles form the player.
+    //needs mouse position to know where to fire
     public void FireProjectile() {
-
-        Debug.LogWarning("Fire!");
+        if ((Time.timeSinceLevelLoad - timeSinceLastShot) >= timeBetweenShots) {
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
+            timeSinceLastShot = Time.timeSinceLevelLoad;
+        }
     }
+
 }
---
+++ Game Studio Project/Assets/Scripts/GameScripts/PlayerScripts/ProjectileMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    [SerializeField]
+    float timeAlive = 3;
+    float t = 0;
+    [SerializeField]
+    float speed = 20;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (t == 1)
+        {
+            Destroy(gameObject);
+        }
+        else {
+            t += Time.deltaTime / timeAlive;
+        }
+        rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Obstacle") {
+            //"delete" asteroid; ask sean how i'm suppose to do this
+            Destroy(gameObject);
+        }
+    }
+}

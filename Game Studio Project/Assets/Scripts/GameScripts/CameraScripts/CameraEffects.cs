cbb6fb11ead027fe2787f93b64f110e757880029
TSF404 <TSF404@users.noreply.github.com>
2019-05-09 13:35:08 +1000

    As a player, I want to have my controls distorted when I go insane, so that the game gets harder.

---
+++ Game Studio Project/Assets/Scripts/GameScripts/CameraScripts/CameraEffects.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+public class CameraEffects : MonoBehaviour
+{
+
+    public static CameraEffects instance;
+    public float currentInsanity = 0;
+
+    //DASH EFFECT
+    public PostProcessVolume dashPost;
+    private Coroutine dashRoutine;
+    public float dashOnLength, dashOffLength;
+
+
+    //DATA MOSH
+    public bool moshEnabled = false;
+    public Kino.Datamosh mosh;
+
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Initialise ()
+    {
+        DashOff();
+    }
+
+
+
+
+    public void ApplyInsanity (float insanity)
+    {
+        currentInsanity = insanity;
+        if (!moshEnabled && insanity > 0.6f)
+        {
+            moshEnabled = true;
+            EnableMosh();
+        }
+        else if (moshEnabled && insanity < 0.6f)
+        {
+            DisableMosh();
+        }
+        if (insanity > 0.6f)
+        {
+            SetMosh(insanity);
+        }
+    }
+
+    private void StopDashCoroutine ()
+    {
+        if (dashRoutine != null)
+        {
+            StopCoroutine(dashRoutine);
+            dashRoutine = null;
+        }
+    }
+
+
+    public void DashOn()
+    {
+        StopDashCoroutine();
+        dashRoutine = StartCoroutine(ToggleDash(true));
+    }
+
+    public void DashOff()
+    {
+        StopDashCoroutine();
+        dashRoutine = StartCoroutine(ToggleDash(false));
+    }
+
+    private IEnumerator ToggleDash (bool state)
+    {
+        float timeElapsed = 0;
+        float length = state ? dashOnLength : dashOffLength;
+        float start = dashPost.weight;
+        float end = state ? 1 : 0;
+        
+        while (timeElapsed < length)
+        {
+            dashPost.weight = Mathf.Lerp(start, end, timeElapsed / length);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        dashPost.weight = end;
+    }
+
+
+    private void EnableMosh()
+    {
+        mosh.Glitch();
+        mosh.enabled = true;
+    }
+
+    private void DisableMosh ()
+    {
+        mosh.enabled = false;
+    }
+
+    public void SetMosh (float insanity) //0 to 1
+    {
+        mosh.blockSize = (int)GlobalMethods.Remap(insanity, 0.5f, 1, 2, 50);
+        mosh.entropy = GlobalMethods.Remap(insanity, 0.6f, 1, 0f, 0.5f);
+    }
+    
+    
+}

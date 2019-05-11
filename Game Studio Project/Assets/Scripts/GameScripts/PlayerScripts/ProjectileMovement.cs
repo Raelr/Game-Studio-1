ce623969c92ad45a3cb5900045bdb404093481d4
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-11 11:09:03 +1000

    As a player, i want to shoot obstacles so that i can survive
    lasers can now get rid of obstacles. Still issues with new movement.

--- Game Studio Project/Assets/Scripts/GameScripts/PlayerScripts/ProjectileMovement.cs
+++ Game Studio Project/Assets/Scripts/GameScripts/PlayerScripts/ProjectileMovement.cs
@@ -10,12 +10,10 @@ public class ProjectileMovement : MonoBehaviour
     [SerializeField]
     float speed = 20;
     Rigidbody rb;
-    private AlternativeArchitecture.GamePooler gp;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        gp = GameObject.FindGameObjectWithTag("GameController").GetComponent<AlternativeArchitecture.GamePooler>();
     }
 
     private void FixedUpdate()
@@ -27,15 +25,16 @@ public class ProjectileMovement : MonoBehaviour
         else {
             t += Time.deltaTime / timeAlive;
         }
-        rb.AddForce(Vector3.forward * speed, ForceMode.Impulse);
+        rb.AddForce(transform.up * speed, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.transform.tag == "Obstacle") {
             //"delete" asteroid; ask sean how i'm suppose to do this
             collision.gameObject.GetComponent<AlternativeArchitecture.Obstacle>().BackToPool();
-            Destroy(gameObject);
         }
+        Destroy(gameObject);
     }
 }

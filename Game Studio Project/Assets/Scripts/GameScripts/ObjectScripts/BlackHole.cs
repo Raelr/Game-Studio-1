7515b66017d553f611d8965da4956d0999ef9b9f
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-05-25 14:07:17 +1000

    As a player, I want to have black holes appear which pull me toward them, so that I can be challenged.

---
+++ Game Studio Project/Assets/Scripts/GameScripts/ObjectScripts/BlackHole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    [SerializeField]
+    private float pullForce;
+    [SerializeField]
+    private Transform player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            player = other.transform;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (player != null) {
+            Vector3 dir = (transform.position - player.position).normalized;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") {
+            player = null;
+        }
+    }
+}

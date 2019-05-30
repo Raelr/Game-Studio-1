f3fcfd1d861065d080c92c70f565e572640c2dba
TSF_SeanSimon <thosesixfaces@gmail.com>
2019-05-30 12:29:56 +1000

    + As a player, I want to have a visual indication of when a milestone has been reached so that I feel that I am progressing.

--- Game Studio Project/Assets/Scripts/GameScripts/UIScripts/ProgressUI.cs
+++ Game Studio Project/Assets/Scripts/GameScripts/UIScripts/ProgressUI.cs
@@ -14,14 +14,71 @@ public class ProgressUI : MonoBehaviour
 
     public TextMesh levelText;
 
+    public Transform ballScalar, barScalar;
 
-    public void SetLevelProgress (float progress) { //0 to 1  //call this from game progression
-        //do the lerping of ball / bar scaling
+    public ParticleSystem winParticle;
 
+    private Color currentLevelCol;
+
+
+
+
+
+/*
+    void Update () {
+
+        SetLevelProgress(tempProg);
+
+        if (tempProg > 1) {
+            tempProg = 0;
+            tempLevel ++;
+            SetLevel(tempLevel);
+        }
+
+        if (Input.GetKey(KeyCode.C))
+            tempProg += 0.02f;
     }
 
-    public void SetLevel (int level) { //call this from game progression
-        levelText.color = ballRenderer.material.color = barRenderer.material.color = levelCols[level - 1 % 7];
-        levelText.text = "// Level " + level;
+
+
+ 
+    float tempProg = 0;
+    int tempLevel = 0;
+
+
+void Start () {
+    SetLevel(0);
+    SetLevelProgress(0);
+}*/
+
+
+
+    public void SetLevelProgress (float progress) { //0 to 1  //call this from game progression
+        progress = Mathf.Clamp(progress, 0, 1);
+        if (progress < 0.3f) {
+            SetLocalScale(ballScalar, progress * (1/0.3f));
+            SetLocalScale(barScalar, 0);
+        }
+        else {
+            SetLocalScale(ballScalar, 1);
+            SetLocalScale(barScalar, new Vector3(1, (progress * (1/0.7f)) - 0.35f, 1));
+        }
+    }
+
+    private void SetLocalScale (Transform obj, float scale) {
+        SetLocalScale(obj, new Vector3(scale, scale, scale));
+    }
+    private void SetLocalScale (Transform obj, Vector3 scale) {
+        obj.localScale = scale;
+    }
+
+    public Color SetLevel (int level) { //start at level 0   call this from game progression. Return the color of the new level
+        levelText.text = "// Level " + (level + 1);
+        if (level != 0) winParticle.Emit(10);
+
+        level = level % (levelCols.Count);
+        currentLevelCol = levelCols[level];
+        levelText.color = ballRenderer.material.color = barRenderer.material.color = currentLevelCol;
+        return currentLevelCol;
     }
 }

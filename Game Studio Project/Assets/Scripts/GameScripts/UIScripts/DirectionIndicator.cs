1471d73cdeed52320d62e116342ff742af8406a1
TSF404 <TSF404@users.noreply.github.com>
2019-06-06 01:41:35 +1000

    As a player, I want to see the high score so I can try to beat it, As a player, I want to see indicators so I will understand where to go if I am lost

---
+++ Game Studio Project/Assets/Scripts/GameScripts/UIScripts/DirectionIndicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionIndicator : MonoBehaviour
+{
+
+    public Transform camTransform;
+    public GameObject leftDir, rightDir, upDir, downDir;
+    public float minThreshold = 55, maxThreshold = 180;
+
+    void Update()
+    {
+        Vector3 angle = camTransform.eulerAngles;
+        Debug.Log(angle);
+
+        leftDir.SetActive(angle.y > minThreshold && angle.y < maxThreshold);
+        rightDir.SetActive(angle.y < 360- minThreshold && angle.y > 360- maxThreshold);
+        upDir.SetActive(angle.x > minThreshold && angle.x < maxThreshold);
+        downDir.SetActive(angle.x < 360 - minThreshold && angle.x > 360 - maxThreshold);
+    }
+}

dd534cb7d276d9875f628f203cc65ee437dede53
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-06-09 18:33:04 +1000

    As a player, i want a count down to appear when the game starts so that I can feel engaged.

---
+++ Game Studio Project/Assets/Scripts/GameScripts/UIScripts/CountDownUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMesh tm;
+    [SerializeField]
+    private Renderer tmrend;
+    private bool counting = false;
+    private float duration = 3.0f;
+    private float t = 0;
+
+    IEnumerator StartCountDown() {
+        while (counting) {
+            if (t >= duration)
+            {
+                StopCounting();
+            }
+            else {
+                int num = (int)(duration - t) + 1;
+                tm.text = "" + num;
+                t += Time.fixedDeltaTime;
+            }
+            yield return null;
+        }
+    }
+
+    public void StopCounting() {
+        counting = false;
+        t = 0;
+        tmrend.enabled = false;
+        StopCoroutine(StartCountDown());
+    }
+
+    public void StartCounting() {
+        t = 0;
+        counting = true;
+        tmrend.enabled = true;
+        StartCoroutine(StartCountDown());
+    }
+}

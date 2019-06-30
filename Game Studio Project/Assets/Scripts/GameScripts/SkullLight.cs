c8d28f21782353f8453fdda6ebf25a9bc617b958
TSF404 <TSF404@users.noreply.github.com>
2019-06-30 11:56:55 +1000

    + Setup skull progress bar

---
+++ Game Studio Project/Assets/Scripts/GameScripts/SkullLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullLight : MonoBehaviour
+{
+    private Renderer render;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+        SetOff();
+    }
+
+    public void SetOn ()
+    {
+        render.material.color = Color.red;
+    }
+
+    public void SetOff()
+    {
+        render.material.color = Color.black;
+    }
+}
